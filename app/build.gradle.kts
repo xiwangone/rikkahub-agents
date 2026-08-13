5c05a934352c3019c79e556ca31caf33ef15170b
re-ovo <1609403959@qq.com>
2026-08-14 00:10:49 +0800

    build: bump version to 2.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 173
-        versionName = "2.4.6"
+        versionCode = 174
+        versionName = "2.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
