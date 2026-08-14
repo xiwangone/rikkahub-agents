4217b5b7b506b21755b1ff6b5d573ab2b733b6a5
re-ovo <1609403959@qq.com>
2026-08-14 17:10:40 +0800

    chore: bump to 2.4.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 175
-        versionName = "2.4.8"
+        versionCode = 176
+        versionName = "2.4.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
